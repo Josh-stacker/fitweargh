b1e961b102437dd6e76d2741741398cd59796c4e
Joshua Obeng.A <joshuaobeng.a@gmail.com>
2026-04-03 10:33:23 +0000

    homepage completed

--- src/components/HeroSlider.tsx
+++ src/components/HeroSlider.tsx
@@ -3,7 +3,7 @@ import herobg from "../assets/hero-bg2.webp";
 function HeroSlider() {
   return (
     <section
-      className="w-full h-[30vh]"
+      className="w-full h-[60vh] min-[1441px]:h-[30vh]"
       style={{
         backgroundImage: `url(${herobg})`,
         backgroundSize: "cover",
